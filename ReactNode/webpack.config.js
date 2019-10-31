1cff089a7f4eed38ba281d77e98d156b11e97736
timmchenadv <timm.chen@advantech.com.tw>
2019-10-31 17:45:20 +0800

    新增Babel、Webpack等套件之組態內容 & 修改NPM套件package.json內容

---
+++ ReactNode/webpack.config.js
@@ -0,0 +1,26 @@
+﻿"use strict";
+
+var path = require("path");
+var WebpackNotifierPlugin = require("webpack-notifier");
+var BrowserSyncPlugin = require("browser-sync-webpack-plugin");
+
+module.exports = {
+    entry: "./Scripts/jsx/index.jsx",
+    output: {
+        path: path.resolve(__dirname, "./Scripts/react"),
+        filename: "bundle.jsx"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ]
+    },
+    devtool: "inline-source-map",
+    plugins: [new WebpackNotifierPlugin(), new BrowserSyncPlugin()]
+};
